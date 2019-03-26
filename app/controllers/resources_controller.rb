c169cbe6764fb163ca701ad75914a303d317aa8b
Andrew Law <andrewlaw8@berkeley.edu>
2019-03-26 11:53:41 -0700

    standard API Get

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -5,12 +5,15 @@ class ResourcesController < ApplicationController
                                       :colleges, :availabilities, :innovation_stages, :topics, :technologies)
   end
 
+  # assumes API GET request in this format :
+  # GET /resources?types=Events,Mentoring&audiences=Undergraduate,Graduate
+  # GET /resources?title=Feminist Research Institute
   def index
-    sort_by = nil
     if params.include? :sort_by
-      puts 'included sort_by'
-      sort_by = params[:sort_by]
-      params.delete :sort_by
+      sort_by = params[:sort_by] # fetch sort_by term
+      params.delete :sort_by # remove sort_by param from params hash
+    else
+      sort_by = nil
     end
 
     @resources = Resource.filter(resource_params).order(sort_by)
@@ -24,6 +27,11 @@ class ResourcesController < ApplicationController
   def show
     id = params[:id]
     @resource = Resource.find_by_id(id)
+
+    respond_to do |format|
+      format.json {render :json => @resource.to_json(:include => Resource.reflect_on_all_associations(:has_many).map! { |association| association.name.to_sym } ) }
+      format.html
+    end
   end
 
   def new
--- app/models/resource.rb
+++ app/models/resource.rb
@@ -13,34 +13,30 @@ class Resource < ActiveRecord::Base
   validates :title, :url, :contact_email, :location, :types, :audiences, :presence => true
   validates :desc, :presence => true, :length => {:maximum => 500}
 
+  # returns a list of all associations [:types, :audiences, :client_tags, :population_focuses, :campuses, ...]
   @@has_many_associations = Resource.reflect_on_all_associations(:has_many).map! { |association| association.name.to_sym }
 
   def self.filter(params)
-    params = params.to_h
-    # Split params into has_many_fields and normal fields
+    params = params.to_h.map {|k,v| [k.to_sym, v]}.to_h # convert ActiveRecord::Controller params into hash with symbol keys
+
+    # Partition params into has_many fields and normal fields
     # has_many_hash = {k => [v1,v2,v3]} ; ex. {audiences => [undergrad, grad, alumni]}
     has_many_hash = {}
-    params = params.map {|k,v| [k.to_sym, v]}.to_h # change keys into symbols
     params.each_key do |key|
       if @@has_many_associations.include?(key)
-        has_many_hash[key] = params[key].split(',') # split by comma delimiter
-        has_many_hash[key] = has_many_hash[key].map do |x| x.strip end # just to be safe, strip leading and trailing whitespace
-        params.delete(key)
+        has_many_hash[key] = params[key].split(',').map { |x| x.strip } # split by comma delimiter, and strip leading and trailing whitespace
+        params.delete(key) # remove has_many key from params
       end
     end
-    puts 'params', params
-    puts 'hmh', has_many_hash
 
     filtered = [] # return value (list of records matching query)
     resources = Resource.where(params) # filter by singular parameters first
 
-    puts 'resources:' , resources
     # return early if there are no has_many fields
     if has_many_hash.empty?
       return resources
     else #todo - find a way to clean up this block. :(  [matching the has_many queries]
-      print 'hmh : ' + has_many_hash.to_s
-      # for each of the candidate resources, check if it matches the has_many queries
+      # for each of the candidate resources filtered by the singular parameters, check if it matches the has_many queries
       resources.find_each do |resource|
         associations_hash = {:types => resource.types, :audiences => resource.audiences, :client_tags => resource.client_tags,
                              :population_focuses => resource.population_focuses, :campuses => resource.campuses, :colleges => resource.colleges,
@@ -50,13 +46,14 @@ class Resource < ActiveRecord::Base
         bool_arr = []
         # for each has_many query, check if the current record's has_many field contains all values in the query
         has_many_hash.each do |field, values|
-          bool_arr.append (associations_hash[field].records.map do |x| x.val end & values).sort == values.sort
+          bool_arr.append (associations_hash[field].records.map { |x| x.val } & values).sort == values.sort
         end
         # if all queries return true, append the record to the returned value
         if bool_arr.all?
           filtered.append resource
         end
       end
+      # return ActiveRecord::Relation instead of array so that ActiveRecord calls can be chained outside this function
       return Resource.where(id: filtered.map(&:id))
     end
   end
