a3345b5b0ed6050ab476a8c23390893762a82767
John Ly <johnly98@berkeley.edu>
2019-03-31 13:22:37 -0700

    added post to controller

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,8 +1,7 @@
 class ResourcesController < ApplicationController
-
   def resource_params
-    params.permit(:title, :url, :contact_email, :location, :types, :audiences, :population_focuses, :campuses,
-                                      :colleges, :availabilities, :innovation_stages, :topics, :technologies)
+    params.permit(:title, :url, :contact_email, :location, :population_focuses, :campuses,
+                                      :colleges, :availabilities, :innovation_stages, :topics, :technologies, :types => [], :audiences => [])
   end
 
   # assumes API GET request in this format :
@@ -39,7 +38,28 @@ class ResourcesController < ApplicationController
   end
 
   def create
-
+    #this should check if any of the params are missing via validation and set an instance variable equal to the missing fields
+    #otherwise add a new object to the database 
+    #redirect to a submission page
+    @error = false
+    #attempt to create and save resource to the database for approval
+    r = Resource.new(resource_params)
+    r.approval_status = 0
+    
+    if r.invalid?
+      @error = true
+      @missing = []
+      @desc_too_long = false
+      Resource.get_required_resources.each do |resource|
+        if !params.include? resource or params[resource] == nil?
+          @missing.push(resource)
+        end
+      end
+      if !params[:desc] == nil and params[:desc].length > 500
+        @desc_too_long = true
+      end
+    end
+    r.save
   end
 
   def update
