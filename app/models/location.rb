4ce01b005f020db61365895aa72476b5f2dd4955
Andrew Law <andrewlaw8@berkeley.edu>
2019-04-01 11:29:25 -0700

    refactor tests

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,4 +2,10 @@ class Location < ActiveRecord::Base
   belongs_to :parent, :class_name => "Location", :foreign_key => "parent_location_id"
   has_many :child, :class_name => "Location", :foreign_key => "child_location_id"
   validates :parent, :presence => true
+
+  # locations = [US, California, Bay Area, Berkeley]
+  def self.seed
+
+  end
+
 end
--- spec/controller/resources_controller_spec.rb
+++ spec/controller/resources_controller_spec.rb
@@ -1,6 +1,5 @@
 require 'rspec'
 require 'rails_helper'
-require 'support/factory_girl'
 
 RSpec.describe ResourcesController, :type => :controller do
   describe 'Responds successfully' do
@@ -14,46 +13,12 @@ RSpec.describe ResourcesController, :type => :controller do
     end
   end
 
-  describe 'model filter method' do
+  describe 'model filter method is called upon GET request' do
     it 'calls the model method that performs the database filtering' do
       params = ActionController::Parameters.new(  {types: "Events,Mentoring"} )
       params.permit!
       expect(Resource).to receive(:filter).with(params)
       get :index, params: {:types => 'Events,Mentoring', :sort_by => 'title'}
-    end
-    it 'can do a simple filter by column name' do
-      Resource.create_resource "title" => "thing1", "url" => "something.com", "contact_email" => "something@gmail.com", "location" => "someplace",
-                               "types" => 'Scholarship,Funding,Events,Networking', "audiences" => 'Grad,Undergrad', "desc" => "descriptions"
-      Resource.create_resource "title" => "thing2", "url" => "something.com", "contact_email" => "something@gmail.com", "location" => "someplace",
-                               "types" => 'Scholarship,Funding,Mentoring', "audiences" => 'Grad,Undergrad', "desc" => "descriptions"
-      Resource.create_resource "title" => "thing3", "url" => "something.com", "contact_email" => "something@gmail.com", "location" => "someplace",
-                               "types" => 'Scholarship,Funding,Events,Networking', "audiences" => 'Grad,Undergrad', "desc" => "descriptions"
-      Resource.create_resource "title" => "thing4", "url" => "something.com", "contact_email" => "something@gmail.com", "location" => "someplace",
-                               "types" => 'Scholarship,Funding,Mentoring', "audiences" => 'Grad,Undergrad', "desc" => "descriptions"
-
-      result = Resource.filter({"title" => "thing3"})
-      print result.pretty_print_inspect
-      expect(result.count).to eq 1
-      expect(result.first.title).to eq "thing3"
-
-      get :index, params: {:types => 'Scholarship,Funding', :sort_by => 'title'}
-      print response.parsed_body
-    end
-    it 'splits has_many associations correctly and returns the right thing' do
-      # create 2 resources with Scholarship and Funding types but with an extra type as well
-      Resource.create_resource "title" => "thing1", "url" => "something.com", "contact_email" => "something@gmail.com", "location" => "someplace",
-          "types" => 'Scholarship,Funding,Events,Networking', "audiences" => 'Grad,Undergrad', "desc" => "descriptions"
-      Resource.create_resource "title" => "thing2", "url" => "something.com", "contact_email" => "something@gmail.com", "location" => "someplace",
-                               "types" => 'Scholarship,Funding,Mentoring', "audiences" => 'Grad,Undergrad', "desc" => "descriptions"
-      Resource.create_resource "title" => "thing3", "url" => "something.com", "contact_email" => "something@gmail.com", "location" => "someplace",
-                               "types" => 'Funding,Mentoring', "audiences" => 'Grad,Undergrad', "desc" => "descriptions"
-
-      result = Resource.filter({:types=>"Scholarship,Funding"})
-      print result.pretty_print_inspect
-      expect(result.count).to eq 2
-      expect(result.where(title: "thing1")).to exist
-      expect(result.where(title: "thing2")).to exist
-      expect(result.where(title: "thing3")).not_to exist
     end
   end
 
