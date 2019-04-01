68b196a90a63229041663b73248c0ef8a5b444a8
Andrew Law <andrewlaw8@berkeley.edu>
2019-04-01 16:27:36 -0700

    refactor model code

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -18,6 +18,15 @@ class Resource < ActiveRecord::Base
     Resource.reflect_on_all_associations(:has_many).map! { |association| association.name.to_sym }
   end
 
+  def self.get_associations_hash(resource)
+    return {:audiences => resource.audiences, :availabilities => resource.availabilities,
+            :campuses => resource.campuses, :client_tags => resource.client_tags,
+            :colleges => resource.colleges, :innovation_stages => resource.innovation_stages,
+            :population_focuses => resource.population_focuses, :technologies => resource.technologies,
+            :topics => resource.topics, :types => resource.types
+    }
+  end
+
   def self.filter(params)
     params = params.to_h.map {|k,v| [k.to_sym, v]}.to_h # convert ActiveRecord::Controller params into hash with symbol keys
     # Partition params into has_many fields and normal fields
@@ -30,35 +39,36 @@ class Resource < ActiveRecord::Base
       end
     end
 
-    filtered = [] # return value (list of records matching query)
     resources = Resource.where(params) # filter by singular parameters first
 
     # return early if there are no has_many fields
     if has_many_hash.empty?
       return resources
-    else #matching the has_many queries is a potential bottleneck! O(NK) sorts and set operations
-          # where N = # records and K = # fields (max 10)]
-          # alternative is to do a 11 way join, and check the existence of each value of the query
-      # for each of the candidate resources filtered by the singular parameters, check if it matches the has_many queries
-      resources.find_each do |resource|
-        associations_hash = {:types => resource.types, :audiences => resource.audiences, :client_tags => resource.client_tags,
-                             :population_focuses => resource.population_focuses, :campuses => resource.campuses, :colleges => resource.colleges,
-                             :availabilities => resource.availabilities, :innovation_stages => resource.innovation_stages, :topics => resource.topics,
-                             :technologies => resource.technologies
-        }
-        bool_arr = []
-        # for each has_many query, check if the current record's has_many field contains all values in the query
-        has_many_hash.each do |field, values|
-          bool_arr.append (associations_hash[field].records.map { |x| x.val } & values).sort == values.sort
-        end
-        # if all queries return true, append the record to the returned value
-        if bool_arr.all?
-          filtered.append resource
-        end
-      end
-      # return ActiveRecord::Relation instead of array so that ActiveRecord calls can be chained outside this function
-      return Resource.where(id: filtered.map(&:id))
+    else
+      return self.filter_has_many_helper(resources, has_many_hash)
     end
+  end
+
+  def self.filter_has_many_helper(resources, has_many_hash)
+    #matching the has_many queries is a potential bottleneck! O(NK) sorts and set operations
+    # where N = # records and K = # fields (max 10)]
+    # alternative is to do a 11 way join, and check the existence of each value of the query
+    # for each of the candidate resources filtered by the singular parameters, check if it matches the has_many queries
+    filtered = [] # list of returned records
+    resources.find_each do |resource|
+      associations_hash = self.get_associations_hash(resource)
+      bool_arr = []
+      # for each has_many query, check if the current record's has_many field contains all values in the query
+      has_many_hash.each do |field, values|
+        bool_arr.append (associations_hash[field].records.map { |x| x.val } & values).sort == values.sort
+      end
+      # if all queries return true, append the record to the returned value
+      if bool_arr.all?
+        filtered.append resource
+      end
+    end
+    # return ActiveRecord::Relation instead of array so that ActiveRecord calls can be chained outside this function
+    return Resource.where(id: filtered.map(&:id))
   end
 
   def self.create_resource(params)
@@ -74,12 +84,7 @@ class Resource < ActiveRecord::Base
     end
 
     resource = Resource.create!(resource_hash)
-    fields_hash = {:audiences => resource.audiences, :availabilities => resource.availabilities,
-                   :campuses => resource.campuses, :client_tags => resource.client_tags,
-                   :colleges => resource.colleges, :innovation_stages => resource.innovation_stages,
-                   :population_focuses => resource.population_focuses, :technologies => resource.technologies,
-                   :topics => resource.topics, :types => resource.types
-    }
+    fields_hash = self.get_associations_hash(resource)
     fields_hash.each do |field, association|
       if params[field] != nil
         params[field].each do |val|
@@ -118,41 +123,13 @@ class Resource < ActiveRecord::Base
     return [location] + self.find_parent_locations(parent&.val)
   end
 
-  # def self.find_parent_resources(location, params)
-  #   if Location.exists?(location) and Location.find(location).parent
-  #     parent = Location.find(location).parent
-  #     params[:location] = parent
-  #     # make a hash of :location to loc, call Resource.filter, then combine the 2 lists, if parent has parent then do
-  #     # recusive call
-  #     resources = self.filter(params)
-  #     resources.or(self.find_parent_resources(parent, params))
-  #     return resources
-  #   end
-  #   return Resource.none
-  # end
-
   #todo verify email and url beforehand?
   def self.validate_email_url(email, url)
   end
 
   def self.get_required_resources
     return ["title", "url", "contact_email", "location", "types", "audiences", "desc"]
-  end 
-
-  # def self.get_has_many_hashes(params)
-  #   has_many_hash = {}
-  #   params.each do |key, value|
-  #     # puts "oof"
-  #     # puts key
-  #     # puts value
-  #     if @@has_many_associations.include?(key.to_sym)
-  #       # puts "loop"
-  #       has_many_hash[key] = params[key].split(',').map { |x| x.strip } # split by comma delimiter, and strip leading and trailing whitespace
-  #       params.delete(key) # remove has_many key from params
-  #     end
-  #   end
-  #   return has_many_hash
-  # end
+  end
 
 end
 
