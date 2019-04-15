3ec3da189b44b0a72c2a2df2e2fbcea513928418
Kostoeva <irmareka@gmail.com>
2019-04-15 11:24:58 -0700

    changing migration

--- db/migrate/20190415181847_create_edits.rb
+++ db/migrate/20190415181847_create_edits.rb
@@ -1,7 +1,7 @@
 class CreateEdits < ActiveRecord::Migration[5.2]
   def change
     create_table :edits do |t|
-      t.integer :id
+      t.integer :resource_id
       t.string :user
       t.string :parameter
       t.timestamps
