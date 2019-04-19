894089ffe68f8e7e6019d8dbe34fb6de64070097
Andrew Law <andrewlaw8@berkeley.edu>
2019-04-19 11:23:39 -0700

    add foreign key indices

--- db/migrate/20190321100624_create_resources.rb
+++ db/migrate/20190321100624_create_resources.rb
@@ -31,60 +31,70 @@ class CreateResources < ActiveRecord::Migration[5.2]
       t.belongs_to :resource      # required
       t.string :val              # type name
       t.timestamps
+      t.index [:resource_id], name: "index_types_on_resource_id"
     end
 
     create_table :audiences do |t|
       t.belongs_to :resource      # required
       t.string :val              # audience type
       t.timestamps
+      t.index [:resource_id], name: "index_audiences_on_resource_id"
     end
 
     create_table :client_tags do |t| # hidden
       t.belongs_to :resource     # NOT required
       t.string :val             # client name
       t.timestamps
+      t.index [:resource_id], name: "index_client_tags_on_resource_id"
     end
 
     create_table :population_focuses do |t|
       t.belongs_to :resource     # NOT required
       t.string :val             # population focus type
       t.timestamps
+      t.index [:resource_id], name: "index_population_focuses_on_resource_id"
     end
 
     create_table :campuses do |t|
       t.belongs_to :resource     # NOT required
       t.string :val             # campus name
       t.timestamps
+      t.index [:resource_id], name: "index_campuses_on_resource_id"
     end
 
     create_table :colleges do |t|
       t.belongs_to :resource     # NOT required
       t.string :val             # college name
       t.timestamps
+      t.index [:resource_id], name: "index_colleges_on_resource_id"
     end
 
     create_table :availabilities do |t|
       t.belongs_to :resource     # NOT required
       t.string :val             # availability type
       t.timestamps
+      t.index [:resource_id], name: "index_availabilities_on_resource_id"
     end
 
     create_table :innovation_stages do |t|
       t.belongs_to :resource     # NOT required
       t.string :val             # innovation stage name
       t.timestamps
+      t.index [:resource_id], name: "index_innovation_stages_on_resource_id"
     end
 
     create_table :topics do |t|
       t.belongs_to :resource    # NOT required
       t.string :val            # topic name
       t.timestamps
+      t.index [:resource_id], name: "index_topics_on_resource_id"
     end
 
     create_table :technologies do |t|
       t.belongs_to :resource    # NOT required
       t.string :val            # technology type
       t.timestamps
+      t.index [:resource_id], name: "index_technologies_on_resource_id"
     end
   end
 end
