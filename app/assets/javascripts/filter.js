c2fb2036dfc26e122ca2c159b867460404d1c79b
Antonio <abfranco4@gmail.com>
2019-05-06 13:42:23 -0700

    remembers selected location

--- app/assets/javascripts/filter.js
+++ app/assets/javascripts/filter.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
     $(".single_checkbox").click(function() {
         if($(this).find('input[type="checkbox"]').prop("checked") == true){
-        $(this).find('input[type="checkbox"]').prop({"checked": false});
-        if ($(this).hasClass("gold_label")) {
-            $(this).children(".label").css({"background-color":"rgb(255, 240, 209)"});
-        } else {
-            $(this).children(".label").css({"background-color":"#D8DFEB"});
-        }
-        } else {
-        $(this).find('input[type="checkbox"]').prop({"checked": true});
-        $(this).children(".label").css({"background-color":"#96CAE7"});
+            $(this).find('input[type="checkbox"]').prop({"checked": false});
+            if ($(this).hasClass("gold_label")) {
+                $(this).children(".label").css({"background-color":"rgb(255, 240, 209)"});
+            } else {
+                $(this).children(".label").css({"background-color":"#D8DFEB"});
+            }
+            } else {
+            $(this).find('input[type="checkbox"]').prop({"checked": true});
+            $(this).children(".label").css({"background-color":"#96CAE7"});
         }
     });
     $(".single_radio").click(function() {
