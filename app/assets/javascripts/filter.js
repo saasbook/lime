1a5df8e5d420503718806f7fecc30dbf962d8170
Antonio <abfranco4@gmail.com>
2019-05-09 20:23:49 -0700

    pagination functionality started

--- app/assets/javascripts/filter.js
+++ app/assets/javascripts/filter.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+
+    /* checkboxes */
     
     $(".single_checkbox").each(function () {
         if($(this).find('input[type="checkbox"]').prop("checked") == true){
@@ -36,4 +38,36 @@ $(document).ready(function() {
             $(this).children(".loc-radio").css({"background-color":"#96CAE7"});
         }
     });
+
+
+    /* pagination */
+
+    let numResources = $(".resource-container").length
+    console.log(numResources);
+    let numPages = Math.ceil(numResources / 10);
+    console.log(numPages);
+
+    for (let i = 2; i <= numPages; i++) {
+        $(".pagination").append('<li class="page-item"><a class="page-link" href="#' + i + '">' + i + '</a></li>');
+    }
+    if (numPages === 1) {
+        $(".pagination").append('<li class="page-item disabled"><a class="page-link" href="#" aria-label="Next"><span aria-hidden="true">&raquo;</span><span class="sr-only">Next</span></a></li>')
+    } else {
+        $(".pagination").append('<li class="page-item"><a class="page-link" href="#" aria-label="Next"><span aria-hidden="true">&raquo;</span><span class="sr-only">Next</span></a></li>')
+    }
+
+    $(".resource-container").addClass("hidden-resource");
+    let count = 0;
+    $(".resource-container").each(function() {
+        if (count < 10) {
+            console.log($(this));
+            $(this).removeClass("hidden-resource");
+            count++;
+        }
+    });
+
+
+
+
+
 });
