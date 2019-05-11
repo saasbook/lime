7f4311f8cdabbad65101456deedfb351bece824c
Antonio <abfranco4@gmail.com>
2019-05-11 15:07:04 -0700

    javascript refactoring

--- app/assets/javascripts/unapproved.js
+++ app/assets/javascripts/unapproved.js
@@ -2,16 +2,13 @@ $(document).ready(function() {
     let unapproved = document.querySelectorAll(".unapproved-resource");
     for (var i = 0; i < unapproved.length; i++) {
         unapproved[i].addEventListener("click", function() {
-
-          if($(this).find('input[type="checkbox"]').prop("checked") == true){
-            $(this).find('input[type="checkbox"]').prop({"checked": false});
+          if($(this).find('input[type=checkbox]').prop("checked") === true){
+            $(this).find('input').prop("checked", false);
             $(this).css({"background-color":"white"});
-
           } else {
-            $(this).find('input[type="checkbox"]').prop({"checked": true});
+            $(this).find('input[type=checkbox]').prop("checked", true);
             $(this).css({"background-color":"#FDB515"});
           }
-          
         });
     }
 });
