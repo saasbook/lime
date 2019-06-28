9d641c1c5ff28a31b61390f1ac47342a5a0b5cd1
Antonio <abfranco4@gmail.com>
2019-06-28 09:45:35 -0700

    postgresql issue

--- Gemfile
+++ Gemfile
@@ -43,11 +43,11 @@ gem 'devise'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
-group :development, :test do
-  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
-  # gem 'sqlite3', '~> 1.3.6'
-end
+# group :development, :test do
+#   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
+#   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+#   gem 'sqlite3', '~> 1.3.6'
+# end
 
 group :test do
   gem 'rspec-rails'
--- app/models/resource.rb
+++ app/models/resource.rb
@@ -76,11 +76,13 @@ class Resource < ActiveRecord::Base
     # return early if there are no has_many fields
     search_regex = ""
       if params[:search].to_s.length != 0
-        search_regex = 'title REGEXP ' + '".*' + params[:search].to_s + '.*" OR desc REGEXP ' + '".*' + params[:search].to_s + '.*"'
+        # search_regex = 'title REGEXP ' + '".*' + params[:search].to_s + '.*" OR desc REGEXP ' + '".*' + params[:search].to_s + '.*"'
+        search_regex = 'title ~* ?'
       end
       params.delete :search
     if has_many_hash.empty?
-      return Resource.where(params).where(search_regex)
+      # return Resource.where(params).where(search_regex)
+      return Resource.where('title ~* :pat', :pat => ".*fem.*")
     else
       resources = Resource.where(params).where(search_regex).includes(*Resource.has_many_associations)
       return self.filter_has_many_helper(resources, has_many_hash)
