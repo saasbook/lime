262950c197d84f3de9e83c1b80f7a5a85283c1eb
Antonio <abfranco4@gmail.com>
2019-07-16 11:29:25 -0700

    hid devise remember checkbox

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,5 @@ Rails.application.routes.draw do
   match 'resources/approve/many' => 'resources#approve_many', via: [:put, :patch, :post], :defaults => {:format => 'json'}
   match 'resources/approve/:id' => 'resources#update', via: [:put, :patch, :post], :defaults => {:format => 'json'}
   root "welcome#index"
-  devise_for :users, controllers: {registrations: 'users/registrations'}
   #For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
