fde83be7e9b590d8650dbe1bff790bd1e63017a5
Antonio <abfranco4@gmail.com>
2019-07-24 13:21:08 -0700

    beginning to add flag functionality

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -97,7 +97,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    puts "this"
      devise_parameter_sanitizer.permit(:sign_up, keys: [:api_token, :registration_key])
   end
 
