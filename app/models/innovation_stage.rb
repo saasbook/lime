484aeb25b325d899ca2f9d49b3256cdde8f74277
Antonio <abfranco4@gmail.com>
2019-09-11 23:30:40 -0700

    equality topic edit

--- app/models/innovation_stage.rb
+++ app/models/innovation_stage.rb
@@ -2,7 +2,7 @@ class InnovationStage < ActiveRecord::Base
   belongs_to :resource
 
   def self.get_values
-    ['Research\-Academia', 'Research', 'Project', 'Startups', 'Clubs']
+    ['Research-Academia', 'Research', 'Project', 'Startups', 'Clubs']
   end
 
   def self.count(label)
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,7 +2,7 @@ class Topic < ActiveRecord::Base
   belongs_to :resource
 
   def self.get_values
-    ['Equality, Human Rights, & Justice', 'Education & Learning',
+    ['Equality & Human Rights & Justice', 'Education & Learning',
      'Space', 'Health & Biotech', 'General', 'Social Impact', 'Health',
      'Climate, Environment, & Oceans', 'Science', 'Engineering',
      'Technology', 'Cities', 'International Development', 'Startups+Business',
