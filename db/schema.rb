30e320aeefaab825d2c501d8ff6292bd5fd83473
Kira13zyh <kira13zyh@berkeley.edu>
2019-11-13 20:53:32 -0800

    added name and email to bug_report

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_25_200034) do
+ActiveRecord::Schema.define(version: 2019_11_14_045106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 2019_06_25_200034) do
     t.boolean "closed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "email"
   end
 
   create_table "campuses", force: :cascade do |t|
